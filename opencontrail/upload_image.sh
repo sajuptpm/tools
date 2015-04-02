c798094acd4654c904236702b8611d67b450d612
Saju Madhavan <sajuptpm@gmail.com>
2015-04-02 18:27:09 +0530

    updated upload_image.sh script

--- opencontrail/upload_image.sh
+++ opencontrail/upload_image.sh
@@ -19,11 +19,20 @@ export OS_PASSWORD=secret123
 export OS_TENANT_NAME=admin
 export OS_AUTH_URL=http://127.0.0.1:35357/v2.0
 
-echo "Downloading cirros-0.3.2-x86_64-disk.img ....."
-wget http://download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img
+image_file=cirros-0.3.2-x86_64-disk.img
+
+if [ -f "$image_file" ]
+then
+    echo "File $image_file exist"
+    echo ""
+else
+    echo "Downloading cirros-0.3.2-x86_64-disk.img ....."
+    echo ""
+    wget http://download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img
+fi
 
 echo "Uploading image cirros-0.3.2-x86_64-disk.img ....."
-glance image-create --progress --name cirros-0.3.2 --disk-format qcow2 --container-format bare --is-public True --file cirros-0.3.2-x86_64-disk.img
+glance image-create --progress --name cirros-0.3.2 --disk-format qcow2 --container-format bare --is-public True --file $image_file
 
 
 
