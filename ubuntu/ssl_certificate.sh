5b0e457c3a387bc60adddf669a9ce6cd1dd7738e
Saju Madhavan <sajuptpm@gmail.com>
2015-04-21 14:24:18 +0530

    Updated ssl certificate script

--- ubuntu/ssl_certificate.sh
+++ ubuntu/ssl_certificate.sh
@@ -5,12 +5,10 @@ cd /etc/ssl/certs
 
 #Download DigiCert Root Certificate "DigiCert High Assurance EV Root CA" from 
 #following site.
-sudo wget https://www.digicert.com/CACerts/DigiCertHighAssuranceEVRootCA.crt
---no-check-certificate
+sudo wget https://www.digicert.com/CACerts/DigiCertHighAssuranceEVRootCA.crt --no-check-certificate
 
 #Unfortunately these are encoded and need to be converted into text.
-sudo openssl x509 -inform DES -in DigiCertHighAssuranceEVRootCA.crt -out
-DigiCertHighAssuranceEVRootCA.crt -text
+sudo openssl x509 -inform DES -in DigiCertHighAssuranceEVRootCA.crt -out DigiCertHighAssuranceEVRootCA.crt -text
 
 #The final step is running c_rehash within "/etc/ssl/certs" to scan directories
 #and take a hash value of each '.pem' and '.crt' file in the directory. It then
